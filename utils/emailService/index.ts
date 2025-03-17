c339ad204b197d5798b784903b086ad84a87767c
annasshah <30356728+annasshah@users.noreply.github.com>
2025-03-18 01:35:15 +0500

    updated email sender url

--- utils/emailService/index.ts
+++ utils/emailService/index.ts
@@ -22,7 +22,7 @@ export const sendEmail = async ({
       html: emailHtml,
     };
 
-    const endpoint = 'https://resend.clinicsanmiguel.com/send-batch-email'; 
+    const endpoint = `${process.env.EMAIL_SENDER_URL}/send-batch-email`; 
 
     const response = await axios.post(endpoint, payload, {
       headers: {

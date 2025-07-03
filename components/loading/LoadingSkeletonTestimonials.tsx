14e7e3cc16042dbd984c9a2a9450879e99ef6015
Aleeza Hussain <aleezahussain237@gmail.com>
2025-07-03 15:09:21 +0500

    build error

--- components/loading/LoadingSkeletonTestimonials.tsx
+++ components/loading/LoadingSkeletonTestimonials.tsx
@@ -4,7 +4,7 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 export  function LoadingSkeletonTestimonials() {
-  // Number of cards to render regardless of screen size
+ 
   const skeletons = [1, 2, 3];
 
   return (
@@ -15,7 +15,8 @@ export  function LoadingSkeletonTestimonials() {
           className="flex-shrink-0 w-full sm:w-[80%] md:w-[50%] lg:w-[33.333%] px-2"
         >
           <div className="bg-[#F4F5F6] p-6 rounded-xl shadow-sm h-[13rem] flex flex-col justify-start text-left">
-            {/* Stars */}
+           
+
             <div className="flex gap-1 mb-2 mt-2">
               {[...Array(5)].map((_, i) => (
                 <Skeleton
@@ -28,7 +29,7 @@ export  function LoadingSkeletonTestimonials() {
               ))}
             </div>
 
-            {/* Review */}
+            
             <Skeleton
               count={3}
               height={12}
@@ -37,7 +38,7 @@ export  function LoadingSkeletonTestimonials() {
               highlightColor="#F3F4F6"
             />
 
-            {/* Name */}
+           
             <Skeleton
               width={80}
               height={10}
@@ -47,7 +48,7 @@ export  function LoadingSkeletonTestimonials() {
 
           
             <div className="absolute bottom-0 right-2 opacity-30 text-[100px] text-gray-300 select-none pointer-events-none">
-              //
+             {"//"}
             </div>
           </div>
         </div>

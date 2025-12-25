499fc96e2c2da837223fb82f7eb54d3b98930a30
aleeza <aleezahussain237@gmail.com>
2025-12-25 17:42:43 +0500

    chnages route

--- app/api/vapi/start/route.ts
+++ app/api/vapi/start/route.ts
@@ -3,13 +3,20 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  const response = await fetch("https://api.vapi.ai/call/", {
+  const response = await fetch("https://api.vapi.ai/call", {
     method: "POST",
     headers: {
-      "Authorization": `Bearer ${process.env.VAPI_PRIVATE_KEY}`,
+      Authorization: `Bearer ${process.env.VAPI_PRIVATE_KEY}`, // PRIVATE KEY
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify({
+      assistant: {
+       assistantId: process.env.NEXT_PUBLIC_CLINIC_VAPI_ASSISTANT_ID!,
+      },
+      transport: {
+        provider: "vapi.websocket",
+      },
+    }),
   });
 
   const data = await response.json();
